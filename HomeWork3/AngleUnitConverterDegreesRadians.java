34cb0302f29427447f12e2eceb396d2d6a9984c7
dr3zz <dr3zz@abv.bg>
2014-05-18 23:15:34 +0300

    HomeWork3

--- HomeWork3/AngleUnitConverterDegreesRadians.java
+++ HomeWork3/AngleUnitConverterDegreesRadians.java
@@ -7,31 +7,26 @@ public class AngleUnitConverterDegreesRadians {
 
 		Scanner input = new Scanner(System.in);
 		int num = input.nextInt();
-		
+
 		for (int i = 0; i < num; i++) {
 			double temp = input.nextDouble();
-			String measure  = input.next();
+			String measure = input.next();
 			if (measure.equals("deg")) {
-				System.out.printf("%.6f rad\n",returnRadians(Double.valueOf(temp))
-						);
+				System.out.printf("%.6f rad\n", returnRadians(temp));
 			} else if (measure.equals("rad")) {
-				System.out.printf("%.6f deg\n",returnDegrees(Double.valueOf(temp)));
+				System.out.printf("%.6f deg\n", returnDegrees(temp));
 			}
-
 		}
+	}
+
+	private static double returnRadians(double temp) {
+		temp = Math.toRadians(temp);
+		return temp;
 
 	}
 
-	private static double returnRadians(double test) {
-		test = Math.toRadians(test);
-		return test;
-
+	private static double returnDegrees(double temp) {
+		temp = Math.toDegrees(temp);
+		return temp;
 	}
-
-	private static double returnDegrees(double test) {
-		test = Math.toDegrees(test);
-		return test;
-
-	}
-
 }
