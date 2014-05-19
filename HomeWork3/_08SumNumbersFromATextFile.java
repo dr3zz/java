e21cdf35ec66585f00f29948208582fa1078e946
dr3zz <dr3zz@abv.bg>
2014-05-19 23:12:57 +0300

    _08SumNumbersFrom

---
+++ HomeWork3/_08SumNumbersFromATextFile.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class _08SumNumbersFromATextFile {
+
+	public static void main(String[] args) throws IOException {
+
+		int sum = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader(
+				"e:/temp/file.txt"));) {
+
+			while (true) {
+				String line = br.readLine();
+				if (line == null) {
+					break;
+				}
+				sum = sum + Integer.parseInt(line);
+			}
+			System.out.println(sum);
+
+		} catch (FileNotFoundException e) {
+			System.err.println("Error");
+		}
+	}
+
+}
