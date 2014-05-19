060c1908e6680c9ad4588d3eecf00e53cf9c9e4c
dr3zz <dr3zz@abv.bg>
2014-05-19 22:30:07 +0300

    07_DaysBetweenTwoDates

---
+++ HomeWork3/_07DaysBetweenTwoDates.java
@@ -0,0 +1,33 @@
+
+
+
+import java.util.Scanner;
+
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class _07DaysBetweenTwoDates {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		String strFirst = input.next();
+		String strSecond = input.next();
+		System.out.println(daysBetween(strFirst, strSecond));
+	}
+
+	private static long daysBetween(String strFirst, String strSecond) {
+		DateTimeFormatter dateFormat = DateTimeFormat.forPattern("dd-MM-yyyy");
+		DateTime firstDate = dateFormat.parseDateTime(strFirst);
+		org.joda.time.LocalDate firstDateLocal = firstDate.toLocalDate();
+		DateTime secondDate = dateFormat.parseDateTime(strSecond);
+		org.joda.time.LocalDate secondDateLocal = secondDate.toLocalDate();
+		Days d = Days.daysBetween(firstDate, secondDate);
+		long daysBetween = d.getDays();
+		return daysBetween;
+
+	}
+
+}
